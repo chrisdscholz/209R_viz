38f263c1916322b10c6342b4ab2c53708a7c8f26
chrisdscholz <153697137+chrisdscholz@users.noreply.github.com>
2024-11-07 20:51:42 -0800

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,6 @@
 </script>
 
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h3>Project 3 Visualization</h3>
 
 <Graphic />
