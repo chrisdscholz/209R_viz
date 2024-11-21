4c50f671089180a70b1fb9d830a1e88721d7c116
chrisdscholz <153697137+chrisdscholz@users.noreply.github.com>
2024-11-20 20:25:17 -0800

    update

---
+++ src/routes/finalProject/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+    import Graphic from '$lib/graphic.svelte';
+    import Writeup from '$lib/writeup.svelte';
+</script>
+
+
+<h3>209R Project 3 Visualization - Chris Scholz</h3>
+
+<Graphic />
+
+<p>Project Github Repo: <a href='https://github.com/chrisdscholz/209R_viz' target="_blank">LINK</a></p>
+
+<h3>Project Writeup</h3>
+
+<Writeup />
